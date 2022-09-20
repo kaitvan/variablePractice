f3b7b16f477d85e35228db2479d62c72e931cf6d
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2022-09-20 16:21:56 -0500

    Update file with three number variables

--- variables.js
+++ variables.js
@@ -1,3 +1,9 @@
+// string variables
 var firstName = "Kaitlyn";
 var nickname = "kvg";
 var email = "kaitlyn@turing.edu";
+
+// number variables
+var heightInInches = 64;
+var dollarsInBank = 80;
+var numberOfPets = 0;
